8dabda23ca1649e121ee623797fcd72faeaf0694
Elan Qisthi <elanqisthi@gmail.com>
2016-05-11 09:30:07 +0700

    improve search params

--- app/bot.rb
+++ app/bot.rb
@@ -10,18 +10,19 @@ module App
 
     def process(input)
       @message = nil
-      case input
+      message_text = input.text
+      user_reply_id = input.chat_id
+      case message_text
       when /CARI/i
-        # should I specified it by user id?
-        input.slice! input.split(' ')[0]
-        opts = { keywords: input.strip!, per_page: 10 }
-        @message = @product_search.search opts
+        @redis.set "chat-#{user_reply_id}", {last_keyword: message_text, current_page: 0}.to_json
+        do_search
       when /BANTU/i, /TOLONG/i, /APA/i
         @message = "CARI <kata kunci> : mencari barang berdasarkan kata kunci\n"
       when /BUSUK/i, /BEGO/i, /TOLOL/i, /ANJING/i, /ASU/i
         @message = 'Omongannya dijaga bro ;)'
       when /LAGI/i
         # TODO : save page to redis server
+        search_more true
       when /TEST/i
         @message = 'Saya online gan! apa yang bisa saya BANTU? :D'
       end
@@ -29,6 +30,14 @@ module App
 
     def reply
       @message ||= 'Maaf, perintah anda tidak saya ketahui. ketik BANTUAN untuk melihat daftar perintah saya!'
+    end
+
+    private
+
+    def do_search(search_more = false)
+      message_text.slice! message_text.split(' ')[0]
+      opts = { keywords: message_text.strip!, per_page: 6 }
+      @message = @product_search.search opts
     end
   end
 end
--- chatbot_server.rb
+++ chatbot_server.rb
@@ -8,24 +8,10 @@ bot_logic = App::Bot.new config.redis
 Telegram::Bot::Client.run(config.tele_token) do |bot|
   bot.listen do |message|
     puts "-> #{message.from.username} : #{message.text}"
-    bot_logic.process message.text
+    bot_logic.process message
     reply = bot_logic.reply
     target = message.chat.id
     bot.api.send_message(chat_id: target, text: reply, parse_mode: 'Markdown', disable_web_page_preview: true)
     puts "##{reply} -> #{target}"
   end
 end
-# bot = TelegramBot.new(token: config.tele_token)
-# bot.get_updates(fail_silently: true) do |message|
-#   puts "@#{message.from.username}: #{message.text}"
-#   command = message.get_command_for(bot)
-
-#   message.reply do |reply|
-#     bot_logic.process command
-#     reply.parse_mode = 'Markdown'
-#     reply.text = bot_logic.reply
-#     reply.disable_web_page_preview = true
-#     puts "sending #{reply.text.inspect} to @#{message.from.username}"
-#     reply.send_with(bot)
-#   end
-# end
