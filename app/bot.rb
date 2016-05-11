bef5ee4036f61cbf186fb2aaf891768cf3e0fec4
Elan Qisthi <elanqisthi@gmail.com>
2016-05-11 15:16:37 +0700

    update help message

--- app/bot.rb
+++ app/bot.rb
@@ -25,12 +25,12 @@ module App
         save_search_term(search_term)
         do_search
       when /BANTU/i, /TOLONG/i, /APA/i
-        @message = "CARI <kata kunci> : mencari barang berdasarkan kata kunci\n"
+        @message = help_message
       when /BUSUK/i, /BEGO/i, /TOLOL/i, /ANJING/i, /ASU/i
         @message = 'Omongannya dijaga bro ;)'
       when /LAGI/i
         do_search true
-      when /TEST/i
+      when /TEST/i, /PING/i
         @message = 'Saya online gan! apa yang bisa saya BANTU? :D'
       end
     end
@@ -40,6 +40,10 @@ module App
     end
 
     private
+
+    def help_message
+      "Halo! saya adalah EQBot, saya bisa membantu kamu untuk mencari barang yang kamu butuhkan di bukalapak.com \n Silahkan CARI nama barang yang kamu inginkan, nanti saya carikan barangnya ya :D"
+    end
 
     def save_search_term(search_term)
       search_term.each do |k, v|
@@ -52,7 +56,7 @@ module App
       page = @redis.hget "chat-#{@user_reply_id}", 'current_page'
       page = @redis.hincrby "chat-#{@user}", 'current_page', 1 if search_more
 
-      keywords.slice! keywords.split(' ')[0]
+      keywords.slice! keywords.split[0]
       opts = { keywords: keywords.strip!, per_page: 6, page: page }
 
       @message = @product_search.search opts
