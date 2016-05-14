589c3f24271d38f5203430e6ecbdb31f1bb14895
Elan Qisthi <elanqisthi@gmail.com>
2016-05-14 11:02:23 +0700

    test search venue

--- app/search/venue_search.rb
+++ app/search/venue_search.rb
@@ -9,21 +9,22 @@ module App
         api_version = Time.now.strftime('%Y%m%d')
         client_id = token['foursquare']['client_id']
         client_secret = token['foursquare']['client_secret']
-        @foursquare_client = Foursquare2::Client.new(client_id: client_id, client_secret: client_secret, aqpi_version: api_version)
+        @foursquare_client = Foursquare2::Client.new(client_id: client_id, client_secret: client_secret, api_version: api_version)
       end
 
       def search(opts = {})
-        return 'Tidak bisa mencari data' unless opts[:query].present? || opts[:ll].present?
+        return 'Tidak bisa mencari data' if opts[:query].nil? || opts[:ll].nil?
         construct_message(@foursquare_client.search_venues(opts).to_hash)
       end
 
       private
 
       def construct_message(response)
-        message = "Hasil pencarian\n"
-        response.each do |v|
-          message << v['location']
+        message = "Tempat oke deket sini\n"
+        response['venues'].each do |v|
+          message << "#{v['name']} - #{v['location']['address']})\n" unless v['location']['address'].nil?
         end
+        message
       end
     end
   end
--- rspec/venue_spec.rb
+++ rspec/venue_spec.rb
@@ -2,26 +2,27 @@ require_relative 'rspec_helper.rb'
 
 describe '#new' do
   it 'initate new instance of App::Search::VenueSearch' do
-    @product_search = App::Search::VenueSearch.new
-    expect(@product_search).to be_an_instance_of App::Search::VenueSearch
+    @venue_search = App::Search::VenueSearch.new
+    expect(@venue_search).to be_an_instance_of App::Search::VenueSearch
   end
 end
 
-# describe '#search' do
-#   it 'search product with product_search' do
-#     @product_search = App::Search::VenueSearch.new
-#     @opts = { keywords: 'sepeda balap', page: 0, per_page: 6 }
-#     @message = @product_search.search @opts
-#     puts @message
-#   end
+describe '#search' do
+  it 'search product with venue_search' do
+    @venue_search = App::Search::VenueSearch.new
+    @ll = '-6.2011036,106.7808587'
+    @opts = { query: 'burger', ll: @ll, limit: 6 } # required params
+    @message = @venue_search.search @opts
+    puts @message
+  end
 
-#   it 'search and paginate up to 3 pages' do
-#     @product_search = App::Search::VenueSearch.new
-#     (0..3).each do |itr|
-#       @opts = { keywords: 'sepeda balap', page: itr, per_page: 6 }
-#       @message = @product_search.search @opts
-#       puts "page #{itr}"
-#       puts @message
-#     end
-#   end
-# end
+  # it 'search and paginate up to 3 pages' do
+  #   @venue_search = App::Search::VenueSearch.new
+  #   (0..3).each do |itr|
+  #     @opts = { keywords: 'sepeda balap', page: itr, per_page: 6 }
+  #     @message = @venue_search.search @opts
+  #     puts "page #{itr}"
+  #     puts @message
+  #   end
+  # end
+end
