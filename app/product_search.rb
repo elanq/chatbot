fb080a7449dfc543eb37e40068955e56816fae87
Elan Qisthi <elanqisthi@gmail.com>
2016-05-27 18:02:24 +0700

    change yaml key

--- app/product_search.rb
+++ app/product_search.rb
@@ -7,7 +7,7 @@ module App
   class ProductSearch
     def initialize
       endpoint = YAML.load(IO.read('app/endpoint.yml'))
-      @uri = URI(endpoint['product_api'])
+      @uri = URI(endpoint['bukalapak_api']['product_search'])
     end
 
     def search(opts = {})
